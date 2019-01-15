e7e2c14a029909d282dc5de57336d354eaeb49e1
Your Name <you@example.com>
2019-01-15 16:11:33 +0800

    -m添加增加敏感词

--- main.c
+++ main.c
@@ -5,19 +5,36 @@ int index = 0;
 int main()
 {
 
-    printf("进入敏感字符系统\n");
+    printf("jrmgzf\n");
     while (1)
     {
-    printf("1---添加敏感字符\n");
-    printf("2---删除敏感字符\n");
-    printf("3---查看敏感字符\n");
-    printf("4---替换敏感字符\n");
-    printf("5---退出\n");
-    printf("请输入相应功能\n");
+    printf("1---tjmgzf\n");
+    printf("2---scmgzf\n");
+    printf("3---ckmgzf\n");
+    printf("4---thmgzf\n");
+    printf("5---tuichu\n");
+    printf("qingshurxiangyanggongneng\n");
     int code;
     scanf("%d", &code);
     if (code == 1)
+    {
+        printf("qingshuruminganzifu\n");
+        char m;
+        scanf("%c",&m);
+        scanf("%c",&m);
+        arr[index]=m;
+        index++;
+        printf("tianjiachenggong,dianhuichejx");
+        char x;
+        scanf("%c",&x);
+        scanf("%c",&x);
+        }
         if (code == 2)
+        {
+            //printf("shanchuzuihuoyigeminganzifu\n");
+            //index--;
+            //printf("shanchuchenggong,dianji")
+        }
             if (code == 3)
                 if (code == 4)
                     if (code == 5)
